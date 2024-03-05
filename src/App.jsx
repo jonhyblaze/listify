f02ca315eac4a430953f1224fd8cc8c910660106
Alexander K <jonhy.blaze@gmail.com>
2024-03-05 20:26:55 +0200

    creating proper loader whenever app is in a loading state while fetching from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,14 @@ function App() {
   };
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex h-screen flex-col justify-center">
+        <h4 className="m-auto inline-flex items-center gap-4 text-2xl text-white">
+          Loading
+          <span className="inline-block h-6 w-6 animate-spin rounded-full border-t-2 border-gray-300 border-opacity-100"></span>
+        </h4>
+      </div>
+    );
   }
 
   return (
